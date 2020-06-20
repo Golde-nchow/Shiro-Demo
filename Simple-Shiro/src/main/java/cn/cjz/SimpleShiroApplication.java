899c8640dcb0055218e1019cbac854e9ba4f1976
Golden_Chow <570384231@qq.com>
2020-06-20 14:22:29 +0800

    创建简单Shiro项目

---
+++ Simple-Shiro/src/main/java/cn/cjz/SimpleShiroApplication.java
@@ -0,0 +1,13 @@
+package cn.cjz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleShiroApplication.class, args);
+	}
+
+}
