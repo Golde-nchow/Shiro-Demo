a93929d9f0e28766a1a3636a68b67596a56d5891
Golden_Chow <570384231@qq.com>
2020-06-21 10:57:34 +0800

    加上MapperScan扫描Mapper接口

--- Simple-Shiro/src/main/java/cn/cjz/SimpleShiroApplication.java
+++ Simple-Shiro/src/main/java/cn/cjz/SimpleShiroApplication.java
@@ -1,9 +1,11 @@
 package cn.cjz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "cn.cjz.dao")
 public class SimpleShiroApplication {
 
 	public static void main(String[] args) {
