658e169e27368fe597f7571b578b87e4855af182
Golden_Chow <570384231@qq.com>
2020-06-24 00:13:07 +0800

    第一种方法：通过定制容器里面的方法，来实现错误页面的跳转

--- Whitelabel-Error-Page_Solution/src/main/java/error/page/WhitelabelErrorPageApplication.java
+++ Whitelabel-Error-Page_Solution/src/main/java/error/page/WhitelabelErrorPageApplication.java
@@ -3,6 +3,11 @@ package error.page;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 /**
  * @author: Kam-Chou
@@ -18,4 +23,16 @@ public class WhitelabelErrorPageApplication {
         SpringApplication.run(WhitelabelErrorPageApplication.class, args);
     }
 
+    /**
+     * 容器定制类，自定义错误页面
+     */
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            ErrorPage unauthorized = new ErrorPage(HttpStatus.UNAUTHORIZED, "unauthorized");
+            ErrorPage errorPage = new ErrorPage(HttpStatus.NOT_FOUND, "404");
+            ErrorPage serverError = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "500");
+            factory.addErrorPages(unauthorized, errorPage, serverError);
+        };
+    }
 }
