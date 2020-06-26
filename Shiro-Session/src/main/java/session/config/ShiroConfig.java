3016b238a7e87213bb4c0733946c6ec94c2847b9
Golden_Chow <570384231@qq.com>
2020-06-26 22:46:16 +0800

    在ShiroConfig添加自定义Session的相关配置

--- Shiro-Session/src/main/java/session/config/ShiroConfig.java
+++ Shiro-Session/src/main/java/session/config/ShiroConfig.java
@@ -2,9 +2,16 @@ package session.config;
 
 import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.session.mgt.eis.JavaUuidSessionIdGenerator;
+import org.apache.shiro.session.mgt.eis.MemorySessionDAO;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
+import org.apache.shiro.session.mgt.eis.SessionIdGenerator;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -54,11 +61,13 @@ public class ShiroConfig {
      * @param myRealm 连接Shiro和数据的桥梁，当Shiro执行操作的时候，都会去Realm获取数据，进行鉴权
      */
     @Bean
-    public SecurityManager securityManager(MyRealm myRealm) {
+    public SecurityManager securityManager(MyRealm myRealm, SessionManager sessionManager) {
         DefaultWebSecurityManager securityManager =  new DefaultWebSecurityManager();
 
         // 设置自定义realm
         securityManager.setRealm(myRealm);
+        // 设置自定义Session管理
+        securityManager.setSessionManager(sessionManager);
 
         return securityManager;
     }
@@ -97,5 +106,57 @@ public class ShiroConfig {
         DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
         advisorAutoProxyCreator.setProxyTargetClass(true);
         return advisorAutoProxyCreator;
+    }
+
+    /**
+     * 配置sessionId生成策略
+     */
+    @Bean
+    public SessionIdGenerator sessionIdGenerator() {
+        return new JavaUuidSessionIdGenerator();
+    }
+
+    /**
+     * 配置 SessionDAO，用于对session的操作进行持久化.
+     * MemorySessionDAO：基于内存会话维护.
+     * EnterpriseCacheSessionDAO：提供了缓存功能的会话，MapCache来实现.
+     */
+    @Bean
+    public SessionDAO sessionDAO(SessionIdGenerator sessionIdGenerator) {
+        MemorySessionDAO memorySessionDAO = new MemorySessionDAO();
+        memorySessionDAO.setSessionIdGenerator(sessionIdGenerator);
+        return memorySessionDAO;
+    }
+
+    /**
+     * 配置Cookie，与其他Cookie要分开
+     */
+    @Bean
+    public SimpleCookie simpleCookie() {
+        SimpleCookie simpleCookie = new SimpleCookie("my-session-cookie");
+        simpleCookie.setHttpOnly(true);
+        simpleCookie.setPath("/");
+        return simpleCookie;
+    }
+
+    /**
+     * 配置会话管理器
+     */
+    @Bean
+    public SessionManager sessionManager(SimpleCookie simpleCookie, SessionDAO sessionDAO) {
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        sessionManager.setSessionIdCookie(simpleCookie);
+        sessionManager.setSessionDAO(sessionDAO);
+
+        // 过期配置，默认30分钟
+        sessionManager.setGlobalSessionTimeout(600000);
+        // 定时检测过期session
+        sessionManager.setSessionIdUrlRewritingEnabled(true);
+        // 每次检测的时间
+        sessionManager.setSessionValidationInterval(60000);
+        // 取消URL重写
+        sessionManager.setSessionIdUrlRewritingEnabled(false);
+
+        return sessionManager;
     }
 }
