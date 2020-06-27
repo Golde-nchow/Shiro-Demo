4955bc1354a9a2905c3901aa45eedf98b6f036ae
Golden_Chow <570384231@qq.com>
2020-06-27 10:26:17 +0800

    修改SpringBoot启动类配置

--- PersonNumber_Control/src/main/java/person/control/PersonControlApplication.java
+++ PersonNumber_Control/src/main/java/person/control/PersonControlApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(value = "cn.cjz.dao")
+@MapperScan(value = "person.control.dao")
 public class PersonControlApplication {
 
 	public static void main(String[] args) {
